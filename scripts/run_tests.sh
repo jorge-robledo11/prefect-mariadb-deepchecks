862a99899e357084f5f171fe6373233c60127302
Jorge Andrés Robledo Ariza <robledo.11@hotmail.com>
2025-05-24 22:56:39 -0500

    Update files

--- scripts/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script para ejecutar los tests del proyecto usando uv y pytest
-set -e
-
-export PREFECT_LOGGING_LEVEL=CRITICAL
-
-# Ejecutar los tests con uv y pytest
-uv run pytest -W ignore tests
